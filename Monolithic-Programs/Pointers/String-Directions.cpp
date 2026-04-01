7f0221cff7920d59abbe1a4635e638a58cc32729
Gerardo Avalos Sanchez <gerhard.avsn0901@gmail.com>
2026-03-31 20:27:59 -0600

    Create String-Directions.cpp for string pointer example
    Add a simple C++ program to demonstrate string pointers

---
+++ Monolithic-Programs/Pointers/String-Directions.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //Entrada y salida en C++
+#include <stdlib.h>//nos comunicamos con el Sistema Operativo para reiniciar la pantalla
+#include <iomanip>
+#include <cstring> // Necesario para strcmp
+
+using namespace std;
+
+int main(){
+    const char *nombre = "Gerardo";
+    cout<<nombre<<endl;
+    cout<<nombre[0]<<endl;
+    return 0;
+}
